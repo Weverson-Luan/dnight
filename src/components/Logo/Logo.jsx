6b0595523a2a441a5d3d07b975d322e9829ef3e4
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-04-01 21:32:06 -0300

    create/component/NetworkStatus: was created tho component in NetworkStatus.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image } from 'react-native'
 
-import { Icons } from '../commom'
+import { Icons } from '../../commom/icons';
 
 const Logo = () => <Image source={Icons.LOGO} />
 
---
+++ src/components/NetworkStatus/NetworkStatus.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import { NetworkConsumer } from 'react-native-offline'
+import styled from 'styled-components'
+
+import i18n from '../i18n'
+import { Styles } from '../../commom/styles'
+
+const Banner = styled.View`
+  position: absolute;
+  top: ${Platform.OS === 'ios' ? 20 : 0};
+  background-color: ${Styles.Color.ERROR};
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 4px 0 4px;
+`
+
+const Status = styled.Text`
+  color: ${Styles.Color.TEXT_PRIMARY};
+`
+
+const NetworkStatus = () => (
+  <NetworkConsumer>
+    {({ isConnected }) =>
+      isConnected ? null : (
+        <Banner>
+          <Status>{i18n.t('messages.networkOffline')}</Status>
+        </Banner>
+      )
+    }
+  </NetworkConsumer>
+)
+
+export default NetworkStatus
