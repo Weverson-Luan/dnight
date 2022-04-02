72b01a167ce1a3c2ba15de7fd84260aaeb0dfaad
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-04-01 21:12:53 -0300

    create/component/PrimaryButton: was created tho component PrimaryButton.

---
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Text, StyleSheet, ViewPropTypes } from 'react-native'
+import { Button, Form } from 'native-base'
+import PropTypes from 'prop-types'
+
+import { Styles } from '../commom'
+
+const PrimaryButton = ({ onPress, title, size, color, radius, variant, height}) => (
+  <Button 
+  style={{borderRadius: radius, height: height}} 
+  variant={variant}
+  onPress={onPress} 
+  size={size} 
+  colorScheme={color}>{title}</Button>
+)
+
+
+PrimaryButton.propTypes = {
+  onPress: PropTypes.func,
+  title: PropTypes.string,
+  size: PropTypes.string,
+  color: PropTypes.string,
+  radius: PropTypes.number,
+  height: PropTypes.number,
+  variant: PropTypes.string
+}
+
+PrimaryButton.defaultProps = {
+  onPress: () => {},
+  title: '',
+  size: 'sm',
+  color: 'default',
+  variant: 'solid',
+  height: 40
+}
+
+export default PrimaryButton
