ed8a585545eb4e63384141fe7014a432a4054b18
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-04-01 23:56:36 -0300

    refactor/App.js: was done refactor in file main in project.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+
+import { NativeBaseProvider, extendTheme } from "native-base";
+
+import Login from "./screens/ScreenLogin";
 
 export default function App() {
+  const theme = extendTheme({
+    components: {
+      Input: {
+        baseStyle: {
+          color: "#212121",
+          _focus: {
+            borderColor: "#a0a0a0",
+          },
+        },
+      },
+    },
+    colors: {
+      error: {
+        300: "#ff9a8b",
+        500: "#f6685e",
+        700: "#be3634",
+      },
+      primary: {
+        300: "#19526C",
+        400: "#003F5C",
+        500: "#003852",
+      },
+      white: {
+        500: "#fff",
+        700: "#d5d5d5",
+      },
+      grey: {
+        500: "#C0C0C0",
+        700: "#808080",
+      },
+    },
+    config: {
+      initialColorMode: "dark",
+    },
+  });
+
+  // StartApp.onStart(); // Start app functions
   return (
-    <View style={styles.container}>
-      <Text>Hellow Friends !!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <Login />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
