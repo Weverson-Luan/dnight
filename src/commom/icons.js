288a1aaaaa287b259ce1a75d5bf39078b6d10842
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-04-01 21:25:34 -0300

    create/component/Icons: was created tho component in Icons.

---
+++ src/commom/icons.js
@@ -0,0 +1,41 @@
+import MENU from '../assets/icons/menu/menu.android.png'
+import LOGO from '../assets/icons/logo/logo.png'
+import MARKER from '../assets/icons/marker/marker.png'
+import REFRESH from '../assets/icons/refresh/refresh.android.png'
+import LOGIN_BACKGROUND from '../assets/images/login-background.jpeg'
+import MAP_MARKER from '../assets/images/map-marker.png'
+import SEARCH from '../assets/icons/search/search.png'
+import BOTTOM_SEARCH from '../assets/icons/bottomSearch/bottomSearch.png'
+import BOTTOM_PROFILE from '../assets/icons/profile/profile.png'
+import BOTTOM_MAP from '../assets/icons/map/map.png'
+import BOTTOM_EVENT from '../assets/icons/events/events.png'
+import SHARE from '../assets/icons/share/share.png'
+import LIKE from '../assets/icons/like/like.png'
+import NOTIFICATION from '../assets/icons/notification/notification.png'
+import INFO from '../assets/icons/info/info.png'
+import MORE from '../assets/icons/more/more.png'
+import FACEBOOK from '../assets/icons/social/facebook.png'
+import GOOGLE from '../assets/icons/social/google.png'
+import TWITTER from '../assets/icons/social/twitter.png'
+
+export default {
+  MENU,
+  LOGO,
+  MARKER,
+  REFRESH,
+  LOGIN_BACKGROUND,
+  MAP_MARKER,
+  SEARCH,
+  BOTTOM_SEARCH,
+  BOTTOM_PROFILE,
+  BOTTOM_MAP,
+  BOTTOM_EVENT,
+  SHARE,
+  LIKE,
+  NOTIFICATION,
+  INFO,
+  MORE,
+  FACEBOOK,
+  GOOGLE,
+  TWITTER,
+}
