195d9c1addd403d48a2de897a64b5ae0fa27dd6e
evertonshow <evertonfran@hotmail.com>
2022-04-14 19:16:11 -0300

    feature/screen: update of styles

--- src/utils/AwesomeAlert.js
+++ src/utils/AwesomeAlert.js
@@ -1,7 +1,10 @@
 class AwesomeAlert {
+  show = message => {
+    alert(message);
+  };
+}
+export default new AwesomeAlert();
 
-    show = (message) => {
-        alert(message)
-    }
-
-} export default new AwesomeAlert()
+// function AwesomeAlert(message) {
+//   return alert(message);
+// }
