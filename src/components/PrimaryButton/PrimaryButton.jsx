635a57d1d78082706643f7d6d5fb486709a4f6c3
evertonshow <evertonfran@hotmail.com>
2022-04-14 19:11:27 -0300

    feature/components: update of styles

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Text, StyleSheet, ViewPropTypes } from 'react-native'
-import { Button, Form } from 'native-base'
-import PropTypes from 'prop-types'
+import React from "react";
+import { Text, StyleSheet, ViewPropTypes } from "react-native";
+import { Button, Form } from "native-base";
+import PropTypes from "prop-types";
 
-
-export const PrimaryButton = ({ onPress, title, size, color, radius, variant, height}) => (
-  <Button 
-  style={{borderRadius: radius, height: height}} 
-  variant={variant}
-  onPress={onPress} 
-  size={size} 
-  colorScheme={color}>{title}</Button>
-)
-
+export const PrimaryButton = ({
+  onPress,
+  title,
+  size,
+  color,
+  radius,
+  variant,
+  height,
+}) => (
+  <Button
+    style={{ borderRadius: radius, height: height }}
+    variant={variant}
+    onPress={onPress}
+    size={size}
+    colorScheme={color}
+  >
+    {title}
+  </Button>
+);
 
 PrimaryButton.propTypes = {
   onPress: PropTypes.func,
@@ -21,15 +30,14 @@ PrimaryButton.propTypes = {
   color: PropTypes.string,
   radius: PropTypes.number,
   height: PropTypes.number,
-  variant: PropTypes.string
-}
+  variant: PropTypes.string,
+};
 
 PrimaryButton.defaultProps = {
   onPress: () => {},
-  title: '',
-  size: 'sm',
-  color: 'default',
-  variant: 'solid',
-  height: 40
-}
-
+  title: "",
+  size: "sm",
+  color: "default",
+  variant: "solid",
+  height: 40,
+};
