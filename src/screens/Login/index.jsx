36a02a3d7e982a1d309b2da7f9253a593fe36694
evertonshow <evertonfran@hotmail.com>
2022-04-13 23:02:17 -0300

    update

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import { Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 
 // import React, { Component } from "react";
@@ -87,51 +85,6 @@ export default function Login({ navigation }) {
   const { spinner, errors, email, password } = loging;
   return (
     <Screen>
-=======
-import React, { useState } from "react";
-import { Text, TouchableOpacity, View, Image } from "react-native";
-import validator from "validator";
-
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
-import { FormControl, Stack, Input } from "native-base";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import Spinner from "react-native-loading-spinner-overlay";
-
-import { PrimaryButton } from "../../components/PrimaryButton/PrimaryButton";
-import { InputError } from "../../components/Input/InputError/InputError";
-import Icons from "../../commom/icons";
-import Constants from "../../commom/constants";
-import Styles from "../../commom/styles";
-import { Logo } from "../../components/Logo/Logo";
-
-import { Container, Header } from "./style";
-
-export default function Screen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState("");
-  const [spinner, setSpinner] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const [showInputError, setShowInputError] = useState("");
-
-  const onSubmit = () => {
-    if (!email) {
-      setShowInputError("email", "errors.empty.email");
-    } else if (!validator.isEmail(email)) {
-      setShowInputError("email", "errors.invalid.email");
-    } else if (!password) {
-      setShowInputError("password", "errors.empty.password");
-    } else if (password.length < Constants.PASSWORD_MIN_LENGTH) {
-      setShowInputError("password", "errors.invalid.smallPassword");
-    } else {
-      // this.firebaseAuth(email, password);
-      alert("usuario pode se logar");
-    }
-  };
-
-  return (
-    <Container>
->>>>>>> develop
       <View
         style={{
           height: getStatusBarHeight(),
@@ -139,33 +92,21 @@ export default function Screen({ navigation }) {
           backgroundColor: Styles.Color.PRIMARY,
         }}
       />
-<<<<<<< HEAD
 
       {/* 
         <NetworkStatus /> */}
 
-=======
->>>>>>> develop
       <View
         style={{
           flex: 1,
           alignItems: "center",
           backgroundColor: Styles.Color.PRIMARY,
-<<<<<<< HEAD
-=======
-          alignItems: "center",
-          justifyContent: "center",
->>>>>>> develop
         }}
         showsVerticalScrollIndicator={false}
       >
         <Header>
           <Logo />
         </Header>
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
         <Spinner visible={spinner} />
 
         <FormControl style={{ width: "80%", marginVertical: 120 }}>
@@ -182,7 +123,6 @@ export default function Screen({ navigation }) {
             <Input
               style={{ color: "#fff", height: 50 }}
               variant={"none"}
-<<<<<<< HEAD
               value={email}
               keyboardType="email-address"
               placeholder={"placeholders.email"}
@@ -191,16 +131,6 @@ export default function Screen({ navigation }) {
               blurconst
               // HandleLogin={false}
               onChangeText={text => handleChange(text, "email")}
-=======
-              keyboardType="email-address"
-              placeholder={"Digite seu email"}
-              returnKeyType="next"
-              autoCapitalize="none"
-              blurOnSubmit={false}
-              onChangeText={text =>
-                console.log(`aqui o texto digitado ${text}`)
-              }
->>>>>>> develop
               placeholderTextColor="#FFF"
               InputLeftElement={
                 <Icon
@@ -228,22 +158,12 @@ export default function Screen({ navigation }) {
             <Input
               style={{ color: "#fff", height: 50 }}
               variant={"none"}
-<<<<<<< HEAD
               value={password}
               secureTextEntry={password}
               placeholder={"placeholders.password"}
               returnKeyType="done"
               returnKeyLabel={"buttons.login"}
               onChangeText={text => handleChange(text, "password")}
-=======
-              secureTextEntry={true}
-              placeholder={"Digite sua senha"}
-              returnKeyType="done"
-              returnKeyLabel={"buttons.login"}
-              onChangeText={text =>
-                console.log(`aqui o texto digitado ${text}`)
-              }
->>>>>>> develop
               placeholderTextColor="#FFF"
               InputLeftElement={
                 <Icon
@@ -256,21 +176,13 @@ export default function Screen({ navigation }) {
               }
               InputRightElement={
                 <Icon
-<<<<<<< HEAD
                   name={password ? "eye-off-outline" : "eye-outline"}
-=======
-                  name={showPassword ? "eye-off-outline" : "eye-outline"}
->>>>>>> develop
                   size={24}
                   style={{
                     marginRight: 10,
                     color: Styles.Color.TEXT_PRIMARY,
                   }}
-<<<<<<< HEAD
                   onPress={() => setLoging({ password: !password })}
-=======
-                  onPress={() => setShowPassword(!showPassword)}
->>>>>>> develop
                 />
               }
             />
@@ -279,12 +191,8 @@ export default function Screen({ navigation }) {
           <InputError error={errors.password} />
 
           <PrimaryButton
-<<<<<<< HEAD
             title={"buttons.login".toUpperCase()}
             onPress={handleLogin}
-=======
-            title={"Entrar ".toUpperCase()}
->>>>>>> develop
             color={"white"}
             size={"lg"}
             radius={100}
@@ -299,13 +207,7 @@ export default function Screen({ navigation }) {
             }}
           >
             <TouchableOpacity
-<<<<<<< HEAD
               onPress={() => navigation.navigate("RECOVER_PASSWORD_SCREEN")}
-=======
-              onPress={() => {
-                navigation.navigate("RECOVER_PASSWORD_SCREEN");
-              }}
->>>>>>> develop
             >
               <Text
                 style={{
@@ -313,28 +215,16 @@ export default function Screen({ navigation }) {
                   color: "#FFF",
                 }}
               >
-<<<<<<< HEAD
                 {"buttons.forgotPassword"}
-=======
-                Esqueceu sua senha?
->>>>>>> develop
               </Text>
             </TouchableOpacity>
 
             <TouchableOpacity
-<<<<<<< HEAD
               onPress={() =>
                 navigation.navigate("REGISTER_SCREEN", {
                   updateMode: false,
                 })
               }
-=======
-              onPress={() => {
-                this.props.navigation.navigate("REGISTER_SCREEN", {
-                  updateMode: false,
-                });
-              }}
->>>>>>> develop
             >
               <Text
                 style={{
@@ -342,18 +232,13 @@ export default function Screen({ navigation }) {
                   color: "#FFF",
                 }}
               >
-<<<<<<< HEAD
                 {"buttons.signUpNow.firstMessage"}
-=======
-                Criar uma conta
->>>>>>> develop
               </Text>
             </TouchableOpacity>
           </View>
         </FormControl>
 
         {
-<<<<<<< HEAD
           <View style={{ marginTop: 20 }}>
             <Text
               style={{
@@ -364,16 +249,12 @@ export default function Screen({ navigation }) {
             >
               {"labels.loginWith"}
             </Text>
-=======
-          <View style={{ marginTop: 10 }}>
->>>>>>> develop
             <View style={{ flexDirection: "row" }}>
               <TouchableOpacity
                 style={{ marginHorizontal: 10 }}
                 onPress={() => {}}
                 activeOpacity={0.7}
               >
-<<<<<<< HEAD
                 {/* <Image
                     source={Icons.GOOGLE}
                     style={{
@@ -394,27 +275,6 @@ export default function Screen({ navigation }) {
                       height: Styles.Metrics.WIDTH * 0.12,
                     }}
                   /> */}
-=======
-                <Image
-                  source={Icons.GOOGLE}
-                  style={{
-                    width: Styles.Metrics.WIDTH * 0.12,
-                    height: Styles.Metrics.WIDTH * 0.12,
-                  }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{ marginHorizontal: 10 }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={Icons.FACEBOOK}
-                  style={{
-                    width: Styles.Metrics.WIDTH * 0.12,
-                    height: Styles.Metrics.WIDTH * 0.12,
-                  }}
-                />
->>>>>>> develop
               </TouchableOpacity>
               <TouchableOpacity
                 style={{ marginHorizontal: 10 }}
