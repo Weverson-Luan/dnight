4645c1052a190a26d64241c0fac3cf8aa9cb1bd5
evertonshow <evertonfran@hotmail.com>
2022-04-14 06:30:01 -0300

    feature/screen: create the file Login

---
+++ src/screens/Login/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import Styles from "../../commom/styles";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${Styles.Color.SCREEN_BACKGROUND};
+  padding-bottom: ${getBottomSpace()}px;
+`;
+export const Header = styled.View`
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: ${Styles.Color.PRIMARY};
+  padding-bottom: 10px;
+`;
