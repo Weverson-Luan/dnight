0043c31d23d780b60a16b82f21914381370a5b5e
evertonshow <evertonfran@hotmail.com>
2022-04-21 19:25:52 -0300

    feature/screen: update of file

--- src/screens/RecoverPassword/index.jsx
+++ src/screens/RecoverPassword/index.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
-import { View, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
 import { FormControl, Stack, Input } from "native-base";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import validator from "validator";
 import Spinner from "react-native-loading-spinner-overlay";
-import analytics from "@react-native-firebase/analytics";
-import auth from "@react-native-firebase/auth";
-// import AppState from "../../api/AppState";
+// import analytics from "@react-native-firebase/analytics";
+// import auth from "@react-native-firebase/auth";
 
 import i18n from "../../i18n";
 import { Styles } from "../../commom/styles";
 import AwesomeAlert from "../../utils/AwesomeAlert";
+import { InputError } from "../../components/Input/InputError";
+import { PrimaryButton } from "../../components/PrimaryButton";
 
-const RecoverPasswordScreen = () => {
+const RecoverPassword = () => {
   const [email, setEmail] = useState("");
   const [spinner, setSpinner] = useState(false);
   const [errors, setErrors] = useState({
@@ -21,9 +22,9 @@ const RecoverPasswordScreen = () => {
   });
 
   useEffect(() => {
-    analytics().logScreenView({
-      screen_name: "RecoverPasswordScreen",
-    });
+    // analytics().logScreenView({
+    //   screen_name: "RecoverPassword",
+    // });
   });
 
   // showInputError = (input, error, callback = null) => {
@@ -40,92 +41,80 @@ const RecoverPasswordScreen = () => {
   // };
 
   const onSubmit = () => {
-    analytics().logEvent("clickedRecoverPassword");
+    // analytics().logEvent("clickedRecoverPassword");
 
     if (!email) {
       setErrors({ email: i18n.t("errors.empty.email") });
     } else if (!validator.isEmail(email)) {
       setErrors({ email: i18n.t("errors.invalid.email") });
     } else {
-      recoverPassword();
+      // recover();
     }
   };
 
-  const recoverPassword = async ({ navigation }) => {
-    setSpinner(true);
+  // const recover = async ({ navigation }) => {
+  //   setSpinner(true);
 
-    auth()
-      .sendPasswordResetEmail(email.trim().toLowerCase())
-      .then(() => {
-        setSpinner(false);
-        AwesomeAlert.show(i18n.t("messages.emailSent"), null, [
-          {
-            text: i18n.t("buttons.ok"),
-            onPress: () => navigation.navigate("Login"),
-          },
-        ]);
-      })
-      .catch(error => {
-        this.setState({ spinner: false });
-        AwesomeAlert.show(i18n.t("errors.generic"), err);
-      });
-  };
+  //   auth()
+  //     .sendPasswordResetEmail(email.trim().toLowerCase())
+  //     .then(() => {
+  //       setSpinner(false);
+  //       AwesomeAlert(i18n.t("messages.emailSent"), null, [
+  //         {
+  //           text: i18n.t("buttons.ok"),
+  //           onPress: () => navigation.navigate("Login"),
+  //         },
+  //       ]);
+  //     })
+  //     .catch(error => {
+  //       setSpinner(false);
+  //       AwesomeAlert.show(i18n.t("errors.generic"), error);
+  //     });
+  // };
 
   return (
     <View
       style={{
+        flex: 1,
         justifyContent: "space-evenly",
         alignItems: "center",
-        flex: 1,
-        backgroundColor: Styles.Color.SCREEN_BACKGROUND,
+        width: "100%",
       }}
     >
-      <NetworkStatus />
+      <Spinner visible={spinner} />
 
-      <View
-        style={{
-          flex: 4,
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          width: "80%",
-        }}
-      >
-        <Spinner visible={spinner} />
-
-        <FormControl style={{ width: "100%" }}>
-          <Stack
-            style={{
-              marginTop: 15,
-              backgroundColor: Styles.Color.TEXT_PRIMARY,
-              borderRadius: 5,
-            }}
-          >
-            <Input
-              style={{ color: "#000" }}
-              value={this.state.email}
-              placeholder={"Digite seu email"}
-              placeholderTextColor={Styles.Color.PLACEHOLDER}
-              keyboardType="email-address"
-              returnKeyType="done"
-              autoCapitalize="none"
-              onChangeText={text => setEmail(text.toLowerCase())}
-              InputLeftElement={
-                <Icon
-                  name="email-outline"
-                  size={24}
-                  style={{
-                    marginHorizontal: 8,
-                    color: errors.email
-                      ? Styles.Color.ERROR
-                      : Styles.Color.PLACEHOLDER,
-                  }}
-                />
-              }
-            />
-          </Stack>
-          <InputError error={errors.email} />
-        </FormControl>
-
+      <FormControl style={{ width: "80%" }}>
+        <Stack
+          style={{
+            marginTop: 15,
+            backgroundColor: Styles.Color.TEXT_PRIMARY,
+            borderRadius: 5,
+          }}
+        >
+          <Input
+            style={{ color: "#000" }}
+            value={email}
+            placeholder={"Digite seu email"}
+            placeholderTextColor={Styles.Color.PLACEHOLDER}
+            keyboardType="email-address"
+            returnKeyType="done"
+            autoCapitalize="none"
+            onChangeText={text => setEmail(text.toLowerCase())}
+            InputLeftElement={
+              <Icon
+                name="email-outline"
+                size={24}
+                style={{
+                  marginHorizontal: 8,
+                  color: errors.email
+                    ? Styles.Color.ERROR
+                    : Styles.Color.PLACEHOLDER,
+                }}
+              />
+            }
+          />
+        </Stack>
+        <InputError error={errors.email} />
         <PrimaryButton
           onPress={onSubmit}
           title={i18n.t("buttons.recoverPassword").toUpperCase()}
@@ -134,9 +123,10 @@ const RecoverPasswordScreen = () => {
           radius={100}
           height={45}
         />
-      </View>
+      </FormControl>
     </View>
+    // </View>
   );
 };
 
-export default RecoverPasswordScreen;
+export default RecoverPassword;
