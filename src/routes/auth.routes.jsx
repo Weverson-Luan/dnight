3c06534a2e2b69ef084c682b011bff977e4b7160
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-08 19:42:27 -0300

    criado: criado configuração de routas.

---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,29 @@
+// react-navigation-stack
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// screes stacks
+import Login from "../screens/Login";
+import RecoverPassword from "../screens/RecoverPassword";
+import { Register } from "../screens/Register";
+import { Events } from "../screens/Events";
+import { Terms } from "../screens/Terms";
+import { ListEvents } from "../screens/ListEvents";
+
+//routes bottomTabs
+import { TabRoutes } from "./TabRoute";
+
+const { Screen, Navigator } = createNativeStackNavigator();
+
+export const AuthRoutes = () => {
+  return (
+    <Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Screen name="Login" component={Login} />
+      <Screen name="RecoverPassword" component={RecoverPassword} />
+      <Screen name="Register" component={Register} />
+      <Screen name="Events" component={Events} />
+      <Screen name="Terms" component={Terms} />
+      <Screen name="Tab" component={TabRoutes} />
+      <Screen name="ListEvents" component={ListEvents} />
+    </Navigator>
+  );
+};
