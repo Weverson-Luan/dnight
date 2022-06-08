29dc94f50bcaa25e5e21b70e649502f0b0c733fd
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-08 19:46:31 -0300

    criado: foi criado o mock, para armazenar algumas informações.

---
+++ src/mocks/data-explorer.js
@@ -0,0 +1,21 @@
+
+export  const data = [
+  { 
+    background: require('../assets/images/slider/party.png'), 
+    name: 'Quero festa', 
+    description: 'Procurando alguma festa para ir?',
+    type: 'party'
+  },
+  { 
+    background: require('../assets/images/slider/pubs.png'), 
+    name: 'Quero bar', 
+    description: 'Aqui você encontra os melhores bares',
+    type: 'pubs'
+  },
+  { 
+    background: require('../assets/images/slider/events.png'), 
+    name: 'Quero sair', 
+    description: 'Está com tédio? Que tal sair um pouco?',
+    type: 'others' 
+  },
+];
