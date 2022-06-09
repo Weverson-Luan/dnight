bc94014d8d78a0b4ccabba1870b1301a48edc5e1
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-09 00:31:07 -0300

    criado: foi listado eventos cadastrados em nossa base de dados do firebase.

--- src/components/CardSpotlight/index.jsx
+++ src/components/CardSpotlight/index.jsx
@@ -1,5 +1,9 @@
 //icons
+import { useEffect, useState } from "react";
 import Entypo from "react-native-vector-icons/Entypo";
+
+//google-firebase
+import database from '@react-native-firebase/database';
 
 //commons
 import { Styles } from "../../common/styles";
@@ -15,14 +19,44 @@ import {
   TextSpotlightLocation, 
 } from "./styles";
 
+
 export function CardSpotlight({data}){
+  const [dataa, setData] = useState([])
+  useEffect(()=> {
+    database()
+    .ref('/events')
+    .once('value')
+    .then(snapshot => {
+      const events_list_id = snapshot.val();
+      const i = 0;
+      // setData(data)
+      const data = snapshot.val();
+      const eventsObject = {
+        eventName: data.eventName,
+        eventDate: data.eventDate,
+        eventTime: data.eventTime,
+        eventAbout: data.eventAbout,
+        eventCity: data.eventCity,
+        eventLocation: data.eventLocation,
+        eventCategory: data.eventCategory,
+        eventImage: data.eventImage,
+        eventPhone: data.eventPhone,
+        eventTicket: data.eventTicket,
+        eventFeatured: data.eventFeatured,
+        eventChatID: data.eventChatID,
+        eventChild: snapshot.key
+      }
+    })
+    .catch((error)=> console.log("error listing events", error))
+  }, [])
+
   return(
     <Wrapper>
       <WrapperImage>
         <Image source={{ uri: data.img}}/>
       </WrapperImage>
       <WrapperSpotlightDesc>
-        <TextSpotlightText>{data.title}</TextSpotlightText>
+        <TextSpotlightText>{data.eventName}ol</TextSpotlightText>
         <WrapperLocation>
           <Entypo name="location-pin" size={18} color={Styles.Color.PRIMARY_DARK} />
           <TextSpotlightLocation>{data.description}</TextSpotlightLocation>
