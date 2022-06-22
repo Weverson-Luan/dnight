479434a61d2b18c46dd114122b94402e79c7238a
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-21 23:27:01 -0300

    feate : boom: foi implementado o fluxo para pedi permisão da localização do usuário.

--- src/screens/Terms/index.jsx
+++ src/screens/Terms/index.jsx
@@ -7,17 +7,24 @@ import database from '@react-native-firebase/database';
 // async-storage
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+//expo-location
+import * as Location from 'expo-location'
 
 import i18n from "../../i18n";
+
+
 import { PrimaryButton } from "../../components/PrimaryButton";
 
-// import AwesomeAlert from "../../utils/AwesomeAlert";
+import AwesomeAlert from "../../utils/AwesomeAlert";
 
 
 import { Content, contextButton, placeholderTerm, Screen } from "./styles";
 
 export function Terms({navigation}) {
   const [terms, setTerms] = useState("Carregando...");
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
 
   const getPrivacyTerms = async () => {
     database()
@@ -43,8 +50,29 @@ export function Terms({navigation}) {
   }
 
   useEffect(()=> {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      };
+
+      let location = await Location.getCurrentPositionAsync({});
+      
+      setLocation(location);
+
+      if(errorMsg){
+        return  AwesomeAlert.show("Permissão de localização negada");;
+      }
+      if(location){
+        const keyLocation = "@positionActual";
+        const transformLocation = JSON.stringify(location);
+        await AsyncStorage.setItem(keyLocation, transformLocation);
+      }
+    })();
     getPrivacyTerms()
-  })
+  },[])
 
   return (
     <Screen>
