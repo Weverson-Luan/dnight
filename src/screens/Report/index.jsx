172170a7ec3d5f8ef93103fc69d5b1340bbbe5c2
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-07-04 08:41:16 -0300

    create 💥 foi criado a a tela de report

---
+++ src/screens/Report/index.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { FormControl, Stack, } from 'native-base';
+import { Text, TextInput } from 'react-native';
+import { PrimaryButton } from '../../components/PrimaryButton';
+import styled from 'styled-components';
+
+//google-firebase
+import database from '@react-native-firebase/database';
+//async-storage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { Styles } from '../../common/styles'
+import i18n from '../../i18n'
+
+const Screen = styled.SafeAreaView` 
+padding: 8px;
+background-color: ${Styles.Color.SCREEN_BACKGROUND};
+`
+const Content = styled.ScrollView` 
+`
+export function Report(){
+  const navigation = useNavigation();
+const [report, setReport] = useState("");
+
+const sendRequest = async () => {
+
+  const userUuid = await AsyncStorage.getItem(process.env.USER_ID);
+
+  if (Object.values(report).length > 5) {
+    try {
+      database().ref('report').push({
+        uid: userUuid ? userUuid : null,
+        create_at: database().getServerTime().getTime(),
+        message: report
+    },
+    alert("Sua denuncia foi registrada")
+    );
+    setReport("")
+    navigation.goBack();
+    } catch (error) {
+      alert("Sua denuncia não foi registrada")
+      console.log("errpr")
+    }
+  } else {
+    alert("Seu texto está muito pequeno.")
+  };
+
+}
+  return(
+    <Screen>
+      <Content>
+          <FormControl>
+              <Stack last
+                  style={{ borderColor: 'transparent', }}>
+                  <Text>{i18n.t('messages.report')}</Text>
+              </Stack>
+              <Stack
+                  last
+                  style={{
+                      marginTop: 15,
+                      borderColor: 'transparent',
+                      backgroundColor: Styles.Color.TEXT_PRIMARY,
+                      borderRadius: 5,
+                  }}>
+                  <TextInput
+                      placeholder={i18n.t('placeholders.report')}
+                      multiline
+                      editable
+                      placeholderTextColor={Styles.Color.GREY}
+                      maxLength={50}
+                      onChangeText={text => setReport(text)}
+                      value={report}
+                      style={{ padding: 10, }}
+                  />
+              </Stack>
+              <Stack last
+                  style={{ marginHorizontal: 15, marginTop: '20%', borderColor: 'transparent' }}>
+                  <PrimaryButton onPress={() => {
+                      sendRequest()
+                  }} title={i18n.t('buttons.sendReport').toUpperCase()}
+                      color={'error'}
+                      size={'lg'}
+                      radius={100}
+                      height={45}
+                  />
+
+              </Stack>
+          </FormControl>
+      </Content>
+    </Screen>
+  )
+}
---
+++ src/screens/Report/styles.js
@@ -0,0 +1,11 @@
+
+import styled from "styled-components/native";
+import { Styles } from '../../common/styles'
+
+
+export const Screen = styled.SafeAreaView` 
+padding: 8px;
+background-color: ${Styles.Color.SCREEN_BACKGROUND};
+`
+export const Content = styled.ScrollView` 
+`
