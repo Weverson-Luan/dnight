4888e5452f9de2472f7fbdcd778acccf68ab0d77
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-07-04 08:35:02 -0300

    feate: 💥 eu como usuário posso logar pela minha conta google.

---
+++ src/service/auth/AuthLogoutGoogleFirebase.js
@@ -0,0 +1,17 @@
+
+import { useNavigation } from "@react-navigation/native";
+import auth from '@react-native-firebase/auth';
+import { AwesomeAlert } from '../../utils/AwesomeAlert';
+
+
+export const authLogoutGoogleFirebase = async () => {
+  const navigation = useNavigation();
+  try {
+    auth()
+    .signOut()
+    .then((_userLogout)=> navigation.navigate("Login"))
+  } catch (error) {
+    console.log("LOHOH", error)
+    AwesomeAlert("Error em sair")
+  }
+}
