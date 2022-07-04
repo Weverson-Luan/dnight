a94f7630132bcf78f235b48820c012939ed9fa1f
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-07-04 08:57:44 -0300

    create 💥 foi criado a tela para mostrar eventos favoritos.

---
+++ src/screens/FavoriteEvents/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "native-base";
+
+
+
+export function FavoriteEvents(){
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+      <Text>Eventos Favoritos</Text>
+    </View>
+  )
+}
