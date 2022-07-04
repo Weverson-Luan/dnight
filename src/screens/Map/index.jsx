d0bf3b1ee402515c26d21fc40f57e99d3d86d43f
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-07-04 08:54:13 -0300

    refactor 🔨 foi feito refatoração na tela de maps

--- src/screens/Map/index.jsx
+++ src/screens/Map/index.jsx
@@ -6,20 +6,15 @@ import MapView from "react-native-maps";
 // import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 //google-firebase
-import database from '@react-native-firebase/database';
+import database from "@react-native-firebase/database";
 
 //async-storage
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { Content, Screen, ViewFilter } from "./styles";
+import { View } from "native-base";
 
-import {
-   Content,
-   Screen, 
-   ViewFilter 
-} from "./styles";
-
-
-export function MapLocation({navigation}) {
+export function MapLocation({ navigation }) {
   const [userPositionActual, setUserPositionActual] = useState({
     coords: {
       accuracy: 0,
@@ -33,37 +28,37 @@ export function MapLocation({navigation}) {
     mocked: false,
     timestamp: 0,
   });
-  const [ marked, setMarked] = useState([]);
-  const eventRef = database().ref('events');
+  const [marked, setMarked] = useState([]);
+  const eventRef = database().ref("events");
 
   const listEventsInMarker = () => {
-    eventRef.once('value').then((snapshot) => {
+    eventRef.once("value").then((snapshot) => {
       const data = snapshot.val();
 
-      let array = marked
-      for (const [_key,value] of Object.entries(data)) {
-        array.push(value)
-        setMarked([...marked,array])
-      };    
+      let array = marked;
+      for (const [_key, value] of Object.entries(data)) {
+        array.push(value);
+        setMarked([...marked, array]);
+      }
     });
   };
 
   /**
    * NAVIGATION PASSED PARAMS
    */
-  const openEventDetails = (params)=> {
+  const openEventDetails = (params) => {
     navigation.navigate("EventsDetails", {
-      event: params
+      event: params,
     });
-  }
+  };
 
-  useEffect(()=> {
+  useEffect(() => {
     listEventsInMarker();
 
     /**
      * SEACRH POSITION
      */
-    const handlePositionLocationActual = async ()=> {
+    const handlePositionLocationActual = async () => {
       const keyLocation = "@positionActual";
       const location = await AsyncStorage.getItem(keyLocation);
       const transformLocation = JSON.parse(location);
@@ -82,38 +77,34 @@ export function MapLocation({navigation}) {
           showsMyLocationButton={false}
           zoomEnabled={true}
           region={{
-            latitude: userPositionActual?.coords?.latitude, 
+            latitude: userPositionActual?.coords?.latitude,
             longitude: userPositionActual?.coords.longitude,
             latitudeDelta: 0.005922,
             longitudeDelta: 0.00421,
           }}
         >
-           {/* FIRST MARKED*/}
-          
-          {
-            marked.map((events)=> (
-              <>
-                {
-                  events?.eventLocation?.lat ?
-                  <MapView.Marker 
-                    key={events.eventDate}
-                    coordinate={{
-                      latitude:  events?.eventLocation?.lat,
-                      longitude: events?.eventLocation?.lng,
-                    }}
-                   onPress={() => openEventDetails(events)}>
+          {/* FIRST MARKED*/}
+
+          {marked.map((events, index) => (
+            <View key={index}>
+              {events?.eventLocation?.lat ? (
+                <MapView.Marker
+                  key={events.eventDate}
+                  coordinate={{
+                    latitude: events?.eventLocation?.lat,
+                    longitude: events?.eventLocation?.lng,
+                  }}
+                  onPress={() => openEventDetails(events)}
+                >
                   <Image
                     source={require("../../assets/images/map-marker.png")}
                     style={{ width: 36, height: 36 }}
                     resizeMode="contain"
-                   />
-                  </MapView.Marker>   
-                  :
-                  null
-                }
-              </>
-            ))
-          }
+                  />
+                </MapView.Marker>
+              ) : null}
+            </View>
+          ))}
         </MapView>
         <ViewFilter>{/* add search here*/}</ViewFilter>
       </Content>
