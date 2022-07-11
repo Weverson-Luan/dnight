7b7b71882f522708f4b18ffddd110f986e4d8abd
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-07-10 23:46:35 -0300

    fix 🚑 melhoria na função de deslogar o usuário da aplicação

--- src/service/auth/AuthLogoutGoogleFirebase.js
+++ src/service/auth/AuthLogoutGoogleFirebase.js
@@ -1,15 +1,21 @@
 
 import { useNavigation } from "@react-navigation/native";
+//async-storage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import auth from '@react-native-firebase/auth';
 import { AwesomeAlert } from '../../utils/AwesomeAlert';
 
 
-export const authLogoutGoogleFirebase = async () => {
+export const authLogoutApp = async () => {
   const navigation = useNavigation();
   try {
+    AsyncStorage.removeItem(process.env.USER_ID);
     auth()
     .signOut()
-    .then((_userLogout)=> navigation.navigate("Login"))
+    .then((_userLogout)=>{
+      navigation.navigate("Login")
+    })
   } catch (error) {
     AwesomeAlert("Error em sair")
   }
